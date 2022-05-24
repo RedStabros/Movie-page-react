7e9b7509712fa0242afadecc6a19150ced7f6b14
MauricioSG <udcarkangel@gmail.com>
2022-05-23 23:10:39 -0500

    commit 3 conect to API

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,35 @@
-import { MoviesGrid } from "./MoviesGrid";
+import { MoviesGrid } from "./components/MoviesGrid";
 import styles from './App.module.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import { MovieDetails } from "./pages/MovieDetails";
+import { LandingPage } from "./pages/LandingPage";
+
 
 export function App() {
   return (
-    <div>
+    <Router>
       <header>
-        <h1 className={styles.tittle}>Movies</h1>
+        <Link to="/"><h1 className={styles.tittle}>Movies</h1></Link>
+        {/* <Link to="/">Home</Link>
+        <Link to="/movie">Movie</Link> */}
       </header>
       <main>
-        <MoviesGrid />
+        
+        <Switch>
+          <Route exact path="/movies/:movieId"><MovieDetails />
+            
+          </Route>
+          <Route path="/">
+            <LandingPage />
+          </Route>         
+        </Switch>
+        
       </main>
-    </div>
+      </Router>
   );
 }
---
+++ src/components/MoviesGrid.jsx
@@ -0,0 +1,28 @@
+import { useEffect, useState } from "react";
+import { MovieCard } from "./MovieCard";
+import styles from "./MoviesGrid.module.css";
+
+export function MoviesGrid() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch("https://api.themoviedb.org/4/discover/movie", {
+      headers: {
+        Authorization:
+          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OGJmOTY4NTYwMTUwMWFjODU0MjEwNGI1YThmM2FhMCIsInN1YiI6IjYyOGMzOTMxNmM4NDkyNzE4N2FlMTJjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wW1QPMuzjYiQKy5KBJeyrCfd5WhS7fbvn-Xr5tf-AG4",
+        "Content-Type": "application/json;charset=utf-8",
+      },
+    })
+      .then((result) => result.json())
+      .then((data) => {
+        setMovies(data.results);
+      });
+    }, []);
+  return (
+    <ul className={styles.moviesGrid}>
+      {movies.map((movie) => (
+        <MovieCard key={movie.id} movie={movie} />
+      ))}
+    </ul>
+  );
+}

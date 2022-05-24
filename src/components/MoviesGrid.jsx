bdd19e868b6a6a11fb53d42a52763cd5fbe2281e
Mauricio Saldarriaga Garcia <86697006+RedStabros@users.noreply.github.com>
2022-05-23 23:24:11 -0500

    Update MoviesGrid.jsx
    security token

--- src/components/MoviesGrid.jsx
+++ src/components/MoviesGrid.jsx
@@ -9,7 +9,7 @@ export function MoviesGrid() {
     fetch("https://api.themoviedb.org/4/discover/movie", {
       headers: {
         Authorization:
-          "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiI3OGJmOTY4NTYwMTUwMWFjODU0MjEwNGI1YThmM2FhMCIsInN1YiI6IjYyOGMzOTMxNmM4NDkyNzE4N2FlMTJjYSIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.wW1QPMuzjYiQKy5KBJeyrCfd5WhS7fbvn-Xr5tf-AG4",
+          "Bearer <<Token>>",
         "Content-Type": "application/json;charset=utf-8",
       },
     })
